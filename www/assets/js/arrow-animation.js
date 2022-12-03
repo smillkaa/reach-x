8f651a8ead9f41cf0166ebb6074c82f45a318d22
selestenarmo <seleste@popularllc.com>
2022-12-02 17:38:17 -0800

    contact animation

--- www/assets/js/arrow-animation.js
+++ www/assets/js/arrow-animation.js
@@ -1,5 +1,24 @@
-$('.round').click(function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $('.arrow').toggleClass('bounceAlpha');
-});
+const hi = document.querySelector("h1");
+
+// Function that splits each letter of text into its own span
+function spanText(text) {
+    const string = text.innerText; 
+    let spaned = ''; // Setting a variable that is an empty string
+    for (let i = 0; i < string.length; i++) {
+        if(string.substring(i, i + 1) === ' ') spaned += string.substring(i, i + 1); // If string is empty, add a letter into it
+        else spaned += '<span>' + string.substring(i, i + 1) + '</span>'; // Then add a span element around it
+    }
+    text.innerHTML = spaned;
+}
+
+spanText(hi);
+
+let animations = document.querySelectorAll('.animation');
+
+// For each span (letter) add a 0.1 delay of animation
+animations.forEach(animation => {
+    let letters = animation.querySelectorAll('span');
+    letters.forEach((letter, i) => {
+        letter.style.animationDelay = (i * 0.1) + 's';
+    })
+})
